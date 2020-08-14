5f347f1e456e6a2782834f3ddbd8d9874e574a87
Suyyash09 <69357341+Suyyash09@users.noreply.github.com>
2020-08-14 19:19:36 +0530

    Add files via upload

---
+++ datastruc.py
@@ -0,0 +1,13 @@
+print("assigning elements in list")
+mylist=["apple","mango","banana"]
+mylist.append("papaya")
+print(mylist)
+
+print("\naccessing elements in tuple")
+mytuple=("c","c++","python")
+print(mytuple[2])
+
+print("\ndeleting different dictionary elements")
+mydict={"c":"easy","c++":"moderate","java":"tough"}
+del mydict["c"]
+print(mydict)
